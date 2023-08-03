596efba578f769f53b7e913b3d235a18bad89f94
b2200356084 <b2200356084@cs.hacettepe.edu.tr>
2023-08-03 11:31:28 +0300

    respons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({children}:{
 }) {
   return (
     <html lang="en">
+    <head>
+        <title>BlogSite</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
       <body>
       {children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,34 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import {GetServerSideProps} from "next";
 import {Post} from "@/app/models/type";
+import Link from "next/link";
 
 export default async function Home() {
     const data = await getData();
     console.log("Data: ", data);
 
-
-
     return (
         <div className={styles.container}>
             <header>
+                <picture>
+                    <Image
+                        src="/foto.jpg"
+                        alt="Profile Picture"
+                        width={60}
+                        height={60}>
+                    </Image>
+                </picture>
                 <h1>Cihan Yakar Blog</h1>
+                <p>developer</p>
             </header>
             <div>
                 <h2>Posts</h2>
                 <ul>
                     {data?.map((post: Post) => (
-                        <li key={post.title}>
-                            <h3>{post.title}</h3>
-                            <p>{post.createdAt.toDateString()}</p>
+                        <li className={styles.list} key={post.title}>
+                            <Link href="#">{post.title}</Link>
+                            <h3>-{post.createdAt.getFullYear()}-</h3>
+                            <h4>{post.createdAt.getMonth()}</h4>
                         </li>
                     ))}
                 </ul>
