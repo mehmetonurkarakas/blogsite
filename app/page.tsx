3e83f99ee646632ad11297ee6d994fef74b67f9b
b2200356084 <b2200356084@cs.hacettepe.edu.tr>
2023-08-03 16:40:22 +0300

    mapped

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,23 @@ import {Post} from "@/app/models/type";
 import Link from "next/link";
 
 export default async function Home() {
-    const data = await getData();
-    console.log("Data: ", data);
+    let sample = await getData();
+    sample.reverse();
+    let date = 0;
 
     return (
         <div className={styles.container}>
+            <nav className={styles.topLinks}>
+
+
+
+
+                <img className={styles.searchIcon} src = "search.svg" alt="My Happy SVG"/>
+
+                <a href="https://www.linkedin.com/in/cihanyakar/" target="_blank">
+                    <img className={styles.linkedinIcon} src = "linkedin-in.svg" alt="My Happy SVG"/>
+                </a>
+            </nav>
             <header>
                 <picture>
                     <Image
@@ -23,15 +35,30 @@ export default async function Home() {
                 <p>developer</p>
             </header>
             <div>
-                <h2>Posts</h2>
                 <ul>
-                    {data?.map((post: Post) => (
-                        <li className={styles.list} key={post.title}>
-                            <Link href="#">{post.title}</Link>
-                            <h3>-{post.createdAt.getFullYear()}-</h3>
-                            <h4>{post.createdAt.getMonth()}</h4>
-                        </li>
-                    ))}
+                    {
+                        sample.map((post: Post) => {
+                            if (date !== post.createdAt.getFullYear()) {
+                                date = post.createdAt.getFullYear();
+                                return (
+                                    <li className={styles.list} key={post.title}>
+                                        <h3 className={styles.customDate}>-{post.createdAt.getFullYear()}-</h3>
+                                        <h4 className={styles.customDate}>{post.createdAt.getDay()}  {post.createdAt.getMonth()}
+                                            <Link href="#"> {post.title}</Link>
+                                        </h4>
+                                    </li>
+                                )
+                            }
+                            return (
+                                <li className={styles.list} key={post.title}>
+                                    <h4 className={styles.customDate}>{post.createdAt.getDay()} {post.createdAt.getMonth()}
+                                        <Link href="#"> {post.title}</Link>
+                                    </h4>
+                                </li>
+                            )
+                        })
+
+                    }
                 </ul>
             </div>
             <footer>
@@ -44,14 +71,25 @@ export default async function Home() {
 async function getData() {
     const sample: Post[] = [
         {
-            createdAt: new Date("2021-01-01"),
-            title: "İlk yazım"
+            createdAt: new Date("2021-11-08"),
+            title: "Birinci yazım Birinci yazım Birinci yazım Birinci yazım Birinci yazım"
         },
         {
-            createdAt: new Date("2021-01-02"),
+            createdAt: new Date("2021-12-06"),
             title: "İkinci yazım"
+        },{
+            createdAt: new Date("2021-12-06"),
+            title: "3 yazım"
+        },{
+            createdAt: new Date("2022-12-06"),
+            title: "4 yazım"
+        },{
+            createdAt: new Date("2022-12-06"),
+            title: "5 yazım"
+        },{
+            createdAt: new Date("2023-12-06"),
+            title: "6 yazım"
         },
     ];
-
     return sample;
 }
